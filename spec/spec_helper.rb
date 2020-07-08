7f21ddbb08f9f4578b9eb2e8dd08ffa79a21c021
guilherassis <guilher.assis@gmail.com>
2020-07-08 08:39:09 -0300

    Automacao SpotApi

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+
+require_relative "services/api"
+require_relative "libs/mongo"
+
+ambiente = ENV["AMBIENTE"]
+CONFIG = YAML.load_file(File.join(Dir.pwd, "./spec/env/#{ambiente}.yaml"))
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+
+# Adicionando um novo metodo na classe String do ruby, irá converter string para BSON ObjectId
+class String
+  def to_mongo_id
+      BSON::ObjectId.from_string(self)
+  end
+end
